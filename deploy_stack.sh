4c33bca8af3c60c9d71b46efa12b81f9c1483732
Matthieu Simon <matthieu.simon@gmail.com>
2017-05-07 14:59:27 +0200

    use docker secrets for prometheus config files

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+echo "Adding prometheus config in docker secrets"
+docker secret create alert.rules ./prometheus/alert.rules
+docker secret create prometheus.yml ./prometheus/prometheus.yml
+docker secret create alertmanager.yml ./prometheus/alertmanager.yml
 
 echo "Deploying stack"
 docker stack deploy func --compose-file docker-compose.yml
